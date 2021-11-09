edb1fcbe15d70f9c02c65229c4d5eb045e7a6eca
Lia Porto <liabarcellos28@gmail.com>
2021-11-08 22:35:47 -0300

    feature: add 'go to register' button

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,10 +1,13 @@
 import React, {useState} from 'react';
+import Button from './components/Button';
 import Login from './pages/Login';
 
 
 function App() {
 
   const [isLogged, setIsLogged] = useState(false);
+
+  const [currentScreen, setCurrentScreen] = useState("login");
 
   const postLoginData = (data:{}) => {
     console.log(data);
@@ -14,10 +17,22 @@ function App() {
     <div className="main">
       <h1>Olá, {!isLogged ? "visitante" : ""}</h1>
 
-      <h2>{!isLogged ? "Login" : "Cadastre-se"}</h2>
       {!isLogged ?
       (
-        <Login submitData={postLoginData}/>
+        currentScreen === "login" ? (
+          <>
+            <h2>Login</h2>
+            <Login submitData={postLoginData}/>
+            <Button mainColor="primary" styleType="outline" onClick={() => setCurrentScreen("register")}>Ainda não tenho cadastro</Button>
+          </>
+        )
+        :
+        (
+          <>
+            <h2>Cadastre-se</h2>
+            <div>placeholder</div>
+          </>
+        )
       ) :
       (
         <div>placeholder</div>
