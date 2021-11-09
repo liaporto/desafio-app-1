99025ad1589dc28f2174863c212dcccb1a8ee21f
Lia Porto <liabarcellos28@gmail.com>
2021-11-08 22:30:36 -0300

    feature: add title, subtitle and global styles

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,16 +1,28 @@
-import React from 'react';
+import React, {useState} from 'react';
 import Login from './pages/Login';
 
 
 function App() {
+
+  const [isLogged, setIsLogged] = useState(false);
 
   const postLoginData = (data:{}) => {
     console.log(data);
   }
 
   return (
-    <div>
-      <Login submitData={postLoginData}/>
+    <div className="main">
+      <h1>Olá, {!isLogged ? "visitante" : ""}</h1>
+
+      <h2>{!isLogged ? "Login" : "Cadastre-se"}</h2>
+      {!isLogged ?
+      (
+        <Login submitData={postLoginData}/>
+      ) :
+      (
+        <div>placeholder</div>
+      )
+      }
     </div>
   );
 }
--- front/src/globalStyle.ts
+++ front/src/globalStyle.ts
@@ -13,6 +13,8 @@ export default createGlobalStyle`
       sans-serif;
     -webkit-font-smoothing: antialiased;
     -moz-osx-font-smoothing: grayscale;
+    
+    background-color: #F4F4F4;
   }
 
   code {
@@ -20,4 +22,22 @@ export default createGlobalStyle`
       monospace;
   }
 
+  h1, h2{
+    color: var(--primary);
+    margin-bottom: 1em;
+  }
+
+  h1{
+    font-size: 2.5em;
+  }
+
+  h2{
+    font-size: 1.3em;
+    font-weight: 400;
+  }
+
+  .main{
+    text-align: center;
+    margin-top: 10vh;
+  }
 `
