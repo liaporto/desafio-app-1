c56f3277f99ee125b0f8f1d113aca59e0f927994
Lia Porto <liabarcellos28@gmail.com>
2021-11-11 16:50:54 -0300

    feature: add User model

---
+++ back/src/models/User.js
@@ -0,0 +1,60 @@
+const { Model } = require("sequelize");
+const DataTypes = require("sequelize");
+const sequelize = require("../config/sequelize");
+
+const User = sequelize.define("User", {
+  cpf: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  pis: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  name: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  email: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  password: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  country: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  state: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  city: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  postalCode: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  street: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  number: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  number: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  additionalInfo: {
+    type: DataTypes.STRING,
+    allowNull: true,
+  },
+});
+
+module.exports = User;
